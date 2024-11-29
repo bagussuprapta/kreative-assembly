f182db4a2a33f2aa6898d4427c416d1e73a0e8d7
bagussuprapta <slickeel@icloud.com>
2024-11-30 07:01:45 +0800

    feat(setup): setup react, tailwind css, and custom font

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      inclusivesans: ["Inclusive Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
