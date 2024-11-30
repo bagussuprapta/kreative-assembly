c6230be8d0afdcb4636d1ae9d495838c9fdc0d09
bagussuprapta <slickeel@icloud.com>
2024-11-30 14:44:37 +0800

    feat(hero-section): create hero section component

---
+++ src/components/interface/Hero.jsx
@@ -0,0 +1,21 @@
+function Hero() {
+  return (
+    <div className="mt-32 mb-[1000px] flex flex-col gap-y-5 md:flex md:flex-row ">
+      <div className=" flex flex-col gap-y-5 md:w-2/3 ">
+        <p className="text-sm text-center text-gray-500 md:text-left">AI-powered insect monitoring tools</p>
+        <h1 className="text-4xl text-center md:text-left">
+          Empowering <br /> Sustainable Growth <br /> Through Precision <br /> Insect Monitoring
+        </h1>
+        <div className="flex gap-x-3 place-self-center md:place-self-start">
+          <button className="px-7 py-4 bg-bugbug-400 text-bugbug-50 rounded-lg focus:outline-none">LEARN MORE</button>
+          <button className="px-7 py-4 border border-bugbug-400 text-bugbug-400 rounded-lg focus:outline-none">GET STARTED</button>
+        </div>
+      </div>
+      <div className="w-60 place-self-center md:w-1/3">
+        <img className="rounded-3xl w-full" src="Hero.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export { Hero };
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,9 +1,11 @@
 import { Navbar } from "../components/interface/Navbar";
+import { Hero } from '../components/interface/Hero';
 
 function LandingPage() {
   return (
-    <div>
+    <div className='px-2 sm:px-4 md:px-6 lg:px-8 xl:px-24'>
       <Navbar />
+      <Hero />
     </div>
   );
 }
