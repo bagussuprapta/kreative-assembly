57aaef93e139294be1485dfa8ed1f6793267abaa
bagussuprapta <slickeel@icloud.com>
2024-11-30 16:04:49 +0800

    style(hero-section): adjust secondary text styling

--- src/components/interface/Hero.jsx
+++ src/components/interface/Hero.jsx
@@ -1,8 +1,8 @@
 function Hero() {
   return (
-    <div className="mt-32 mb-[1000px] flex flex-col gap-y-5 md:flex md:flex-row ">
+    <div className="mt-32 flex flex-col gap-y-5 md:flex md:flex-row">
       <div className=" flex flex-col gap-y-5 md:w-2/3 justify-between">
-        <p className="text-sm text-center text-gray-500 md:text-left">AI-powered insect monitoring tools</p>
+        <p className="text-sm text-center text-gray-500 md:text-left md:text-lg">AI-powered insect monitoring tools</p>
         <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl xl:leading-tight text-center md:text-left">
           Empowering <br /> Sustainable Growth <br /> Through Precision <br /> Insect Monitoring
         </h1>
