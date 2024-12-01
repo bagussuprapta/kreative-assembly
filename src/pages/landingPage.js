34eb5c389533e3e3ee6262a72d69ba6b07e4fca9
bagussuprapta <slickeel@icloud.com>
2024-12-01 10:45:27 +0800

    feat(review): add review component with carousel cards

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -2,9 +2,10 @@ import { Navbar } from "../components/interface/Navbar";
 import { Hero } from "../components/interface/Hero";
 import { AboutUs } from "../components/interface/AboutUs";
 import { OurSolution } from "../components/interface/OurSolution";
+import { Review } from "../components/interface/Review";
 import { InsightsAndInnovations } from "../components/interface/InsightsAndInnovations";
 import { GetInTouch } from "../components/interface/GetInTouch";
-import { Footer } from '../components/interface/Footer';
+import { Footer } from "../components/interface/Footer";
 
 function LandingPage() {
   return (
@@ -18,6 +19,9 @@ function LandingPage() {
       </div>
       <div className="px-2 sm:px-4 md:px-6 lg:px-8 xl:px-24">
         <OurSolution />
+      </div>
+      <div className="px-2 sm:px-4 md:px-6 lg:px-8 xl:px-24">
+        <Review />
       </div>
       <div className="bg-white px-2 py-11 sm:px-4 md:px-6 lg:px-8 xl:px-24">
         <InsightsAndInnovations />
