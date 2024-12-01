ecd002240cb2ff39217661d428e68221ccea605b
bagussuprapta <slickeel@icloud.com>
2024-12-01 12:31:17 +0800

    fix(hero): layout for mobile

--- src/components/interface/Hero.jsx
+++ src/components/interface/Hero.jsx
@@ -1,18 +1,18 @@
 function Hero() {
   return (
-    <div className="mt-32 flex flex-col gap-y-5 md:flex md:flex-row">
-      <div className=" flex flex-col gap-y-5 md:w-2/3 justify-between">
-        <p className="text-sm text-center text-gray-500 md:text-left md:text-lg">AI-powered insect monitoring tools</p>
-        <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl xl:leading-tight text-center md:text-left">
+    <div className="mt-32 flex flex-col gap-y-5 sm:flex sm:flex-row">
+      <div className=" flex flex-col gap-y-5 justify-between sm:w-2/3 md:w-2/3">
+        <p className="text-base text-center text-gray-500 md:text-left md:text-lg xl:text-xl">AI-powered insect monitoring tools</p>
+        <h1 className="text-4xl  md:text-5xl lg:text-6xl text-center md:text-left xl:text-7xl">
           Empowering <br /> Sustainable Growth <br /> Through Precision <br /> Insect Monitoring
         </h1>
         <div className="flex gap-x-3 place-self-center md:place-self-start">
-          <button className="px-7 py-4 bg-bugbug-600 text-bugbug-50 rounded-lg focus:outline-none">LEARN MORE</button>
-          <button className="px-7 py-4 border border-bugbug-600 text-bugbug-600 rounded-lg focus:outline-none">GET STARTED</button>
+          <button className="px-5 py-3 text-sm bg-bugbug-600 text-bugbug-50 rounded-lg focus:outline-none lg:px-7 lg:py-4 xl:px-10 xl:py-5">LEARN MORE</button>
+          <button className="px-5 py-3 text-sm border border-bugbug-600 text-bugbug-600 rounded-lg focus:outline-none lg:px-7 lg:py-4 xl:px-10 xl:py-5">GET STARTED</button>
         </div>
       </div>
-      <div className="w-60 place-self-center md:w-1/3 xl:">
-        <img className="rounded-3xl w-full" src="Hero.png" alt="" />
+      <div className="w-full place-self-center sm:w-1/3 md:w-1/3 xl:">
+        <img className="rounded-2xl w-full h-64 object-cover object-center sm:h-full" src="Hero.png" alt="" />
       </div>
     </div>
   );
