e9548b4cfaf0612f9f1947b4c7593adbea936937
bagussuprapta <slickeel@icloud.com>
2024-12-01 16:12:51 +0800

    fix(about-us): title wight

--- src/components/interface/AboutUs.jsx
+++ src/components/interface/AboutUs.jsx
@@ -5,7 +5,7 @@ function AboutUs() {
         <img className="rounded-2xl w-full object-cover" src="AboutUs.jpg" alt="" />
       </div>
       <div className="order-1 justify-between flex flex-col gap-y-5 md:w-1/2 md:order-2">
-        <h2 className="text-4xl lg:text-4xl xl:text-5xl xl:leading-tight text-center md:text-left">About US</h2>
+        <h2 className="text-3xl lg:text-4xl xl:text-5xl xl:leading-tight text-center md:text-left">About US</h2>
         <p className="text-center px-3 text-base md:text-left md:px-0">
           Bug Bug Consulting pioneers eco-conscious, AI-driven solutions for insect monitoring in agriculture. Our cutting-edge tools blend sophisticated artificial intelligence with an organic,
           earth-inspired approach, creating sustainable practices that benefit both crop yields and the environment. With a minimalist, eco-modern design philosophy, our tools are crafted to simplify
