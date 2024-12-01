65a152f18746a8a5b63e605f59fa89f052f39abe
bagussuprapta <slickeel@icloud.com>
2024-12-01 15:48:07 +0800

    fix(about-us): order in desktop

--- src/components/interface/AboutUs.jsx
+++ src/components/interface/AboutUs.jsx
@@ -1,10 +1,10 @@
 function AboutUs() {
   return (
     <div className="flex flex-col gap-y-5 pt-8 pb-2 sm:pb-4 md:flex md:flex-row md:gap-x-20 md:py-10 xl:py-24">
-      <div className="order-2 mt-4 place-self-center md:w-1/2">
+      <div className="order-2 mt-4 place-self-center md:w-1/2 md:order-1">
         <img className="rounded-2xl w-full object-cover" src="AboutUs.jpg" alt="" />
       </div>
-      <div className="order-1 flex flex-col gap-y-5 md:w-1/2 justify-between">
+      <div className="order-1 justify-between flex flex-col gap-y-5 md:w-1/2 md:order-2">
         <h2 className="text-4xl lg:text-4xl xl:text-5xl xl:leading-tight text-center md:text-left">About US</h2>
         <p className="text-center px-3 text-base md:text-left md:px-0">
           Bug Bug Consulting pioneers eco-conscious, AI-driven solutions for insect monitoring in agriculture. Our cutting-edge tools blend sophisticated artificial intelligence with an organic,
